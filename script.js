4bb1328a6bfc475e4c20af645ad85b4d13d1e02a
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-01-01 15:21:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+const button=document.getElementById("incrementBtn");
+const para=document.getElementById("counter");
+
+button.addEventListener("click",(e)=>{
+	para.textContent=parseInt(para.textContent)+1
+	alert(parseInt(para.textContent)>0?parseInt(para.textContent)-1:para.textContent)
+})
